3506618e908f7557b507ae6a961f4d482eb260ed
TERRIGW10 <martin.knapp@npl.co.uk>
2022-02-21 12:54:09 +0000

    Drivers updated with code i've written in the SPI test for MAX board

--- Drivers/MAX11300/MAX11300Hex.h
+++ Drivers/MAX11300/MAX11300Hex.h
@@ -1,4 +1,4 @@
-﻿/*******************************************************************************
+/*******************************************************************************
 * Copyright (C) 2017 Maxim Integrated Products, Inc., All Rights Reserved.      
 *                                                                               
 * Permission is hereby granted, free of charge, to any person obtaining a       
