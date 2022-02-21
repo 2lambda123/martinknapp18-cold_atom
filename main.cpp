90b31d3be3532378e6ab5b1fd75ed7b2208f2fb4
TERRIGW10 <martin.knapp@npl.co.uk>
2022-02-21 13:10:47 +0000

    added comments for where coldatom functions will go

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include "mbed.h"
 #include "cycle_count_delay.h"
+#include "coldatom.h"
 
 // main() runs in its own thread in the OS
 int main() {
-    
+
     enableCycleCounter();
 
+    // coldatom_init();
     while (true) {
-        
+        // coldatom_run();
 
     }
 }
