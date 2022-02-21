e89b07b76ac30a2faebd5a0a6d42ca197edb19f1
TERRIGW10 <martin.knapp@npl.co.uk>
2022-02-21 18:03:50 +0000

    working state machine with switch function, prints state every 2 secs

--- coldatom.cpp
+++ coldatom.cpp
@@ -1,2 +1,56 @@
 #include "cycle_count_delay.h"
 #include "coldatom.h"
+#include "mbed.h"
+#include "serial.h"
+
+//BufferedSerial pc(CONSOLE_TX, CONSOLE_RX);
+
+void coldatom_init()
+{
+    printf("Initialising\n\r");
+    return;
+}
+
+
+/* Various states */
+typedef enum tSTATE { 
+    STATE_A,
+    STATE_B,
+    STATE_C 
+} tSTATE;
+
+tSTATE  STATE;
+void coldatom_run()
+{
+    switch(STATE)
+    {
+        case (STATE_A):
+        {
+            printf("STATE_A\n\r");
+            cycle_delay_ms(2000);
+            STATE = STATE_B;
+            break;
+        }
+
+        case (STATE_B):
+        {
+            printf("STATE_B\n\r");
+            cycle_delay_ms(2000);
+            STATE = STATE_C;
+            break;
+        }
+
+        case (STATE_C):
+        {
+            printf("STATE_C\n\r");
+            cycle_delay_ms(2000);
+            STATE = STATE_A;
+            break;
+        }
+
+        default:
+            STATE = STATE_A;
+            break;
+    }
+
+}
--- coldatom.h
+++ coldatom.h
@@ -1,5 +1,7 @@
 #ifndef _COLDATOM_H_
 #define _COLDATOM_H_
 
+void coldatom_init();
+void coldatom_run();
 
 #endif // _COLDATOM_H_
