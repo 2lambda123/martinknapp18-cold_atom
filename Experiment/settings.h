d35cbcb279a2eef4e7dd952595ac6ce32dc3aeab
TERRIGW10 <martin.knapp@npl.co.uk>
2022-02-23 15:17:16 +0000

    estimated rough values for the AOMs, and tidied up

--- Experiment/settings.h
+++ Experiment/settings.h
@@ -1,26 +1,10 @@
-// Analog values
-constexpr float AO1_MOT = 6.6;
-constexpr float AO2_MOT = 1.76;
-constexpr float AO3_MOT = 5;
-constexpr float EO_MOT = 8.93;
-constexpr float NS_MOT = 0.7;
-constexpr float WE_MOT = 1.2;
-constexpr float BIAS_MOT = 0.11;
+#ifndef _SETTINGS_H_
+#define _SETTINGS_H_
 
+// Initial Values
+constexpr float AOM_1_FREQ = 2.5;
+constexpr float AOM_1_ATTEN = 0;
+constexpr float AOM_2_FREQ = 2.5;
+constexpr float AOM_2_ATTEN = 0;
 
-#define K_UP 1
-
-// DDS Paramaeters
-// TODO(split out for different experiment and maybe pass in as object)
-
-// #include "dds_params/declare_T_80_fringes.h"
-// #include "dds_params/declare_T_120_fringes.h"
-// #include "dds_params/declare_T_120_16_fringes.h"
-// #include "dds_params/declare_T_115_fringes_17e_7.h"
-#if K_UP
-#include "dds_params/T_130_fringes.h"
-#include "dds_params/declare_spectroscopy.h"
-#else
-#include "dds_params/k_down.h"
-#include "dds_params/k_down_spec.h"
-#endif
+#endif // _SETTINGS_H_
