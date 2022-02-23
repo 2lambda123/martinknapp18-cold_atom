1b8f3cc10eb1dcd7e00a3519ad4133901b6fdaf9
TERRIGW10 <martin.knapp@npl.co.uk>
2022-02-23 10:03:55 +0000

    get_userinput() now deals with pointer to array rather than array itself

--- Misc/serial.h
+++ Misc/serial.h
@@ -1,4 +1,7 @@
 #include "mbed.h"
+#include <cstdint>
+#include <cstring>
+#include <string>
 
 #ifndef SERIAL_H
 #define SERIAL_H
@@ -7,25 +10,29 @@
 #define BUFFER_SIZE 32
 BufferedSerial pc(CONSOLE_TX, CONSOLE_RX);
 
-void get_userinput(){
+void get_userinput(char* COMMAND_[BUFFER_SIZE]){
 
     char BUFFER[BUFFER_SIZE];
-    char COMMAND[BUFFER_SIZE];
+    char* BUFFERptr = *COMMAND_;
 
     printf("User Input: \n\r");
-        for (int i = 0; i <= BUFFER_SIZE; i++){
-            pc.read(BUFFER, BUFFER_SIZE);
-            COMMAND[i] = BUFFER[0];
-            pc.write(BUFFER, 1);
-            // printf("BUFFER: %s\n\r", BUFFER);
-            // printf("COMMAND: %s\n\r", &COMMAND[i]);
 
-            if (COMMAND[i] == '\r'){
-                COMMAND[i] = '\0';
-                printf("Function Entered: %s\n\r", COMMAND);
-                break;
+        for (int i = 0; i <= BUFFER_SIZE; i++){
+            //read the char typed, save it in command, then print char back to terminal
+            pc.read(BUFFER, BUFFER_SIZE);
+            *BUFFERptr = BUFFER[0];
+            pc.write(BUFFER, 1);
+
+            // was enter pressed?
+            if (*BUFFERptr == '\r'){
+                *BUFFERptr = '\0';
+                printf("Function Entered: %s\n\r", *COMMAND_);
+                break; 
             }
+            BUFFERptr++;
+
         }
+
 }
 
 
