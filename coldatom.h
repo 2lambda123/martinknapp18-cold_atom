0b8ee0ef817f21c305846e36c3da043607d2829e
TERRIGW10 <martin.knapp@npl.co.uk>
2022-02-24 12:52:09 +0000

    PGC_Ramp definition now belongs here

--- coldatom.h
+++ coldatom.h
@@ -2,21 +2,29 @@
 #define _COLDATOM_H_
 
 #include "mbed.h"
+#include "Pin_Assignment.h"
 #include "Drivers/MAX11300/max11300.h"
 
-void coldatom_pins();
+
 void coldatom_init();
 void coldatom_precomp();
 void coldatom_run();
 void coldatom_PGC();
 void coldatom_MOT_Temp();
 
-// class Coldatom{
+// Ramps
+MAX11300::RampAction PGC_Ramp;
+
+// class coldatom{
 //     public:
-//     protected:
+//     coldatom();
 
 //     // Analog Outputs
-//     MAX11300::MAX11300_Ports AOM_1_FREQ;
+//     MAX11300::MAX11300_Ports AOM_1;
+
+//     // Ramps
+//     MAX11300::RampAction PGC_Ramp;
+
 // };
 
 #endif // _COLDATOM_H_
