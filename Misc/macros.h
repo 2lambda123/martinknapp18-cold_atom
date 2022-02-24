9dd2e09ae703e5c00531af67881039f1a8aab7b6
TERRIGW10 <martin.knapp@npl.co.uk>
2022-02-24 12:51:10 +0000

    modified the array size macro, more simple

--- Misc/macros.h
+++ Misc/macros.h
@@ -1,3 +1,1 @@
-#define ARRAYSIZE(a)                                                           \
-  ((sizeof(a) / sizeof(*(a))) /                                                \
-   static_cast<size_t>(!(sizeof(a) % sizeof(*(a)))))
+#define ARRAYSIZE(a) (sizeof(a) / sizeof(*(a)))
