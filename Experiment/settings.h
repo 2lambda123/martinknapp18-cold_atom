d8fc6b1e8c6d5abfc49a179bee21adff1a9beb27
TERRIGW10 <martin.knapp@npl.co.uk>
2022-02-24 12:49:24 +0000

    added to_dac function to convert voltage to decimal for DAC

--- Experiment/settings.h
+++ Experiment/settings.h
@@ -1,7 +1,17 @@
+#include "mbed.h"
+
 #ifndef _SETTINGS_H_
 #define _SETTINGS_H_
 
-// Initial Values
+#define V_REF   10
+#define RES     4095
+
+// Function to convert voltage to decimal value for DAC
+constexpr int16_t to_dac(double voltage){
+  return static_cast<int16_t>(voltage / V_REF * RES);
+}
+
+// Values for precomputing ramps
 constexpr float AOM_1_FREQ = 2.5;
 constexpr float AOM_1_ATTEN = 0;
 constexpr float AOM_2_FREQ = 2.5;
