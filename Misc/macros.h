fd2c56908a5bd96ee0fac9fba207503d9e515c62
TERRIGW10 <martin.knapp@npl.co.uk>
2022-02-24 10:57:20 +0000

    created macros file to use ARRAYSIZE()

---
+++ Misc/macros.h
@@ -0,0 +1,3 @@
+#define ARRAYSIZE(a)                                                           \
+  ((sizeof(a) / sizeof(*(a))) /                                                \
+   static_cast<size_t>(!(sizeof(a) % sizeof(*(a)))))
