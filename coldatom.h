3ffac9e990eab594762f0ba246c16345621e023b
TERRIGW10 <martin.knapp@npl.co.uk>
2022-03-25 18:14:04 +0000

    re-formatting

--- coldatom.h
+++ coldatom.h
@@ -27,14 +27,8 @@ class COLDATOM
     double pd_fraction_;
     uint32_t atom_number_;
 
-    // Create the MAX11300 object
-    MAX11300 MAX11300;
-
-    // Digital Outputs
-    // const uint32_t COOLING_SHUTTER_TTL;
-    // const uint32_t liquid_crystal_1_;
-
     // Analog Outputs
+    MAX11300::MAX11300_Ports TEST_;
     MAX11300::MAX11300_Ports AOM_1_FREQ_;
     MAX11300::MAX11300_Ports AOM_1_ATTE_;
     MAX11300::MAX11300_Ports AOM_2_FREQ_;
