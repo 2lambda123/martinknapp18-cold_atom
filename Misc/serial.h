6eefffa5a2b7f287f7fe22c9df715e98516e8b25
TERRIGW10 <martin.knapp@npl.co.uk>
2022-10-11 10:39:00 +0100

    added serial_init function, user input function for python

--- Misc/serial.h
+++ Misc/serial.h
@@ -1,4 +1,5 @@
 #include "mbed.h"
+#include <cstdint>
 
 #ifndef SERIAL_H
 #define SERIAL_H
@@ -9,32 +10,75 @@ BufferedSerial pc(CONSOLE_TX, CONSOLE_RX);
 
 
 // Provide it with an empty array and the array will be filled with user input
-void get_userinput(char COMMAND_[]){
+void serial_initialize()
+{
+    char BUFFER[BUFFER_SIZE];
+    char COMMAND[BUFFER_SIZE];
+    char *BUFFERptr = COMMAND;
 
+    while (true)
+    {
+        if (pc.readable() == 1)
+        {
+            pc.read(BUFFER, BUFFER_SIZE);
+            *BUFFERptr = BUFFER[0];
+            // was enter pressed?
+            if (*BUFFERptr == '\0')
+            {
+                if (strcmp(COMMAND,"init") == 0)
+                {
+                    break;  
+                }
+            }
+            BUFFERptr++;
+        }
+    }
+}
+
+
+// Provide it with an empty array and the array will be filled with user input
+void get_userinput(char COMMAND_[])
+{
     char BUFFER[BUFFER_SIZE];
     char *BUFFERptr = COMMAND_;
 
-    printf("User Input: \n\r");
-
-        for (int i = 0; i <= BUFFER_SIZE; i++){
-            //read the char typed, save it in command, then print char back to terminal
-            pc.read(BUFFER, BUFFER_SIZE);
-            *BUFFERptr = BUFFER[0];
-            pc.write(BUFFER, 1);
-
-            // was enter pressed?
-            if (*BUFFERptr == '\r'){
-                *BUFFERptr = '\0';
-                printf("Function Entered: %s\n\r", COMMAND_);
-                break; 
-            }
-
-            //inrement pointer to next address in COMMAND_
-            BUFFERptr++;
-
+    while (true)
+    {
+        pc.read(BUFFER, BUFFER_SIZE);
+        *BUFFERptr = BUFFER[0];
+        // was enter pressed?
+        if (*BUFFERptr == '\0')
+        {
+            *BUFFERptr = '\0';
+            // printf("Finished\r\n");
+            printf("Function Entered: %s\n\r", COMMAND_);
+            break; 
         }
-
+        BUFFERptr++;
+    }
 }
+
+
+// // Provide it with an empty array and the array will be filled with user input
+// void get_userinput(char COMMAND_[]){
+//     char BUFFER[BUFFER_SIZE];
+//     char *BUFFERptr = COMMAND_;
+//     // printf("User Input: \n\r");
+//         for (int i = 0; i <= BUFFER_SIZE; i++){
+//             //read the char typed, save it in command, then print char back to terminal
+//             pc.read(BUFFER, BUFFER_SIZE);
+//             *BUFFERptr = BUFFER[0];
+//             // pc.write(BUFFER, 1);
+//             // was enter pressed?
+//             if (*BUFFERptr == '\r'){
+//                 *BUFFERptr = '\0';
+//                 // printf("Function Entered: %s\n\r", COMMAND_);
+//                 break; 
+//             }
+//             //inrement pointer to next address in COMMAND_
+//             BUFFERptr++;
+//         }
+// }
 
 // When an error is required to be processed, this function should be called with the required error code as a variable
 void error_handler(int errorcode){ 
@@ -43,7 +87,6 @@ void error_handler(int errorcode){
         case 1:
             printf("Invalid Function\r\n");
             return;
-
         case 2:
             printf("Invalid Value\r\n");
             return;
