c40aba71fc8acb7cfca71bb89360d31d6238e8df
TERRIGW10 <martin.knapp@npl.co.uk>
2022-10-20 21:44:25 +0100

    Alvium TTL added

--- Misc/Pin_Assignment.h
+++ Misc/Pin_Assignment.h
@@ -20,7 +20,8 @@ MAX11300 MAX11300(MAX11300_SPI, PE_4, NC, NC);
 DigitalOut COOLING_TTL(PG_2),
     REPUMP_TTL(PG_3),
     COIL_TTL(PD_3),
-    MAKO_TTL(PC_3);
+    MAKO_TTL(PC_3),
+    ALVIUM_TTL(PC_2);
 
 // Digital In
 // DigitalIn  CMOS_FRAME_TTL(PC_2);
