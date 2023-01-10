ba7ff4dabdf8cae3411c1b8e7ec2c76c2c3dfc05
G216NQ3 <martin.knapp@npl.co.uk>
2023-01-10 15:17:07 +0000

    added in delay constants for shutters

--- Misc/settings.h
+++ Misc/settings.h
@@ -15,6 +15,12 @@ constexpr int16_t to_dac(double voltage)
 
 // Variables
 
+// SHUTTER VALUES (in us)
+constexpr uint8_t MECH_DELAY_OPEN = 6000;
+constexpr uint8_t MECH_DELAY_CLOSE = 4000;
+constexpr uint8_t AOM_DELAY_OPEN = 15;
+constexpr uint8_t AOM_DELAY_CLOSE = 5;
+
 // MOT PHASE
 constexpr float MOT_LOCK_FREQ = 0.832;
 constexpr float MOT_TRAP_FREQ = 2.000; 
@@ -23,6 +29,8 @@ constexpr float MOT_REPUMP_FREQ = 7.14;
 constexpr float MOT_LOCK_ATTE = 1.3;
 constexpr float MOT_TRAP_ATTE = 1.3;
 constexpr float MOT_REPUMP_ATTE = 1.3;
+
+constexpr float MOT_C_FIELD_ = 0;
 
 // PGC PHASE
 constexpr float PGC_LOCK_FREQ = 0.832;
@@ -41,6 +49,8 @@ constexpr float DETECT_REPUMP_FREQ = 7.14;
 constexpr float DETECT_LOCK_ATTE = 1.3;
 constexpr float DETECT_TRAP_ATTE = 0.9;
 constexpr float DETECT_REPUMP_ATTE = 1.3;
+
+constexpr float DETECT_C_FIELD_ = 1;
 
 // Integer Values
 constexpr uint8_t num_images = 5; // images to take during MOT Temp measurement
