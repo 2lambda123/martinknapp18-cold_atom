6c682fe998f9bd847a9aaa2bc118419c1a7451a7
G216NQ3 <martin.knapp@npl.co.uk>
2023-04-02 22:07:05 +0100

    plotting on the fly

--- Python/Serial_Test.py
+++ Python/Serial_Test.py
@@ -10,10 +10,10 @@ import serial.tools.list_ports
 
 import plotting
 
-
-os.chdir('C:/Users/MAC1/OneDrive - National Physical Laboratory/DPhil/Experimental Results/Python_Test/Data')
+date = '15.02.23'
+os.chdir('C:/Users/MAC1/OneDrive - National Physical Laboratory/DPhil/Experimental Results/Detection/2023/'+date+'')
 cwd = os.getcwd()
-date = '13.10.22'
+print (cwd)
 
 ports = serial.tools.list_ports.comports()
 portList = []
@@ -23,18 +23,10 @@ for onePort in ports:
 print("")
 
 
-BAUD = 9600
-ser = serial.Serial('COM4', BAUD, timeout=(0))
+ser = serial.Serial('COM7', baudrate=921600, timeout=(0))
 print("Enter your commands below.\r\nInsert 'exit' to leave the application.\r\nInsert 'init' to initialize the board.")
-
-functions = [['MOT_TEMP',0],
-          ['MOT_CYCLE',0],
-          ['DROP_TEST',0],
-          ['DROP_CYCLE',0],
-          ['DIAGNOSTIC',1],
-          ['EXPERIMENTAL',1]]
-
-MCU_FUNC = pd.DataFrame(data=functions, columns=['FUNCTION','DATA'])
+# settings = ser.get_settings()
+# print(ser.baudrate)
 
 def switch(action):
     
@@ -49,23 +41,32 @@ def switch(action):
     
     elif action == "read":
         
+        # # printing to terminal on the fly
+        # read_buffer_done_ = ""
+        # read_buffer_ = ""
+        # while ( read_buffer_done_[-4:] != "DONE" ):  
+        #     out = ser.read(1).decode('utf-8')
+        #     read_buffer_ += out
+        #     read_buffer_done_ += out
+            
+        #     if ( read_buffer_[-2:] == "\n\r" ):
+        #         print (">> " + read_buffer_)
+        #         read_buffer_ = ""
+                
+
+        # printing to terminal after function has finished
         read_buffer_ = ""
         data = []
         while ( read_buffer_[-4:] != "DONE" ):
             out = ser.read(1).decode('utf-8')
             read_buffer_ += out
-            
-            # if ( read_buffer_[-2:] == "\n\r" ):
-            #     print (">> " + read_buffer_)
-            #     read_buffer_ = ""
-                
-            
+    
         print (">> " + read_buffer_)
         
         # if there is data to read, read it
         if 'DATA' in read_buffer_:
             # isolate just the part that contains the data
-            read_buffer_ = read_buffer_[read_buffer_.find('DATA')+7 : -4]
+            read_buffer_ = read_buffer_[read_buffer_.find('DATA')+7 :]
             # print (read_buffer_)
             
             # find the number of rows
@@ -73,22 +74,48 @@ def switch(action):
             # n = int(len(read_buffer_)/rows)
             
             # remove the \n and \r chracters
+            read_buffer_ = read_buffer_.replace('DATA', '')
             read_buffer_ = read_buffer_.replace('\n', '')
             read_buffer_ = read_buffer_.replace('\r', '')
             
             # convert the data to array and then dataframe
             data = read_buffer_.split(',')
             data = data[:-1]
+            # print (data)
             df = pd.DataFrame(data)
 
-            # save the data to txt file
-            filename = input(">> Please add filename: ")
-            df.to_csv(''+cwd+'/'+filename+'_serial.txt', header=None, index=None, sep='\t')
-
-            # Do you want to plot?
-            plot = input(">> Plot? (y/n): ")
-            if (plot == 'y'):
-                plotting.data_plot(filename)
+            # # save the data to txt file
+            # filename = input(">> Please add filename: ")
+            # df.to_csv(''+cwd+'/'+filename+'.txt', header=None, index=None, sep='\t')
+            # # plotting.data_plot(filename)
+            
+            # # Do you want to plot?
+            # plot = input(">> Plot? (Y/N): ")
+            # if (plot == 'Y'):
+            #     plotting.data_plot_file(str(cwd), filename)
+            
+            # Just plot
+            plotting.data_plot(df)
+            
+            # # save the data to txt file
+            # N_shots = 5 # no. of shots per variable
+            # N_variable_X = 27 # total number of variables
+            # N_total = N_shots * N_variable_X
+            # for i in range(N_total):
+            #     start = (127 + 2) * (i)
+            #     stop = (127 + 2) * (i+1)
+            #     df = pd.DataFrame(data[start:stop - 2])
+            #     variable_X_index = data[stop-2]
+            #     shot_index = data[stop-1]
+            #     # print (df)
+            #     # print (variable_X_index)
+            #     # print (shot_index)
+                
+            #     filename = ''+str(variable_X_index)+'_'+str(shot_index)+''
+            #     variable = 'PGC_TRAP_FREQ'
+            #     path = ''+cwd+'/'+variable+'/'+filename+'.txt'
+            #     df.to_csv(''+path+'', header=None, index=None, mode='w', sep='\t')
+            #     # plotting.data_plot( str(path), filename)
                 
 
             
--- Python/plotting.py
+++ Python/plotting.py
@@ -10,8 +10,8 @@ from matplotlib import gridspec
 
 
 #--------- path definitons ---------
-os.chdir('C:/Users/MAC1/OneDrive - National Physical Laboratory/DPhil/Experimental Results/Python_Test/Data')
-cwd = os.getcwd()
+# os.chdir('C:/Users/MAC1/OneDrive - National Physical Laboratory/DPhil/Experimental Results/Python_Test/Data')
+# cwd = os.getcwd()
 # print (cwd)
 
 
@@ -24,9 +24,9 @@ RES = 4095
 def ADC_CONVERSION(ADC_CODE_):
     return (ADC_CODE_/RES)*V_REF
 
-def data_plot(filename_):
+def data_plot_file(path_, filename_):
     # import data
-    data = pd.read_csv(''+cwd+'/'+filename_+'_serial.txt', header=None, skiprows=None, names=['ADC_CODE'])
+    data = pd.read_csv(''+path_+'/'+filename_+'.txt', header=None, skiprows=None, names=['ADC_CODE'])
     
     # convert ADC code to voltage
     data['V'] = ADC_CONVERSION(data['ADC_CODE'])
@@ -35,6 +35,40 @@ def data_plot(filename_):
     # plot
     fig1, ax1 = plt.subplots()
     ax1.plot(data['V'],'.-', label='raw data')
+    ax1.set_xlabel(r'index')
+    ax1.set_ylabel(r'Voltage / V')
+    ax1.legend(loc='best')
+    ax1.grid()
+    plt.show()
+    
+    return
+
+def data_plot(dataframe_):
+    # import data
+    data = dataframe_
+    
+    # convert ADC code to voltage
+    data.columns = ['ADC_CODE']
+    data['ADC_CODE'] = pd.to_numeric(data['ADC_CODE']) # convert from strings to integer or float
+    # print (data)
+    data['V'] = ADC_CONVERSION(data['ADC_CODE'])
+    # print (data['V'])
+    
+    # calculate the fraction
+    ADC_SAMPLES = 127
+    # N4 = np.sum(data['ADC_CODE'][0:ADC_SAMPLES])
+    # N34 = np.sum(data['ADC_CODE'][ADC_SAMPLES:2*ADC_SAMPLES])
+    # BG4 = np.sum(data['ADC_CODE'][2*ADC_SAMPLES:3*ADC_SAMPLES])
+    # BG34 = np.sum(data['ADC_CODE'][3*ADC_SAMPLES:4*ADC_SAMPLES])
+    # fraction = float((N4 - BG4) / (N34 - BG34))
+    # print (N4, N34, BG4, BG34, fraction)
+    
+    # plot
+    fig1, ax1 = plt.subplots()
+    ax1.plot(data['V'],'.-', label='raw data')
+    
+    # ax1.plot([], [],'',label='Fraction=%.5f' %(fraction))
+    
     ax1.set_xlabel(r'index')
     ax1.set_ylabel(r'Voltage / V')
     ax1.legend(loc='best')
