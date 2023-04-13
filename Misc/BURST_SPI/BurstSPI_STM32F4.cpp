d570c973e407bb0b1e443be14657862545747f4e
G216NQ3 <martin.knapp@npl.co.uk>
2023-04-13 17:07:19 +0100

    updates with PT, now include the interrupt halting

--- Misc/BURST_SPI/BurstSPI_STM32F4.cpp
+++ Misc/BURST_SPI/BurstSPI_STM32F4.cpp
@@ -31,7 +31,8 @@ void BurstSPI::fastWrite_three_byte(int data) {
         spi->DR = reg_data_buf[i];
     }
 
-    while ((spi->SR & SPI_SR_BSY) == 0); // wait until BSY
+    while ((spi->SR & SPI_SR_TXE) == 0);
+    // while ((spi->SR & SPI_SR_BSY) == 0); // wait until BSY
     while ((spi->SR & SPI_SR_BSY) != 0); // wait until finished with BSY
 }
 
@@ -39,37 +40,69 @@ void BurstSPI::fastWrite_three_byte(int data) {
 uint16_t BurstSPI::fastRead( int reg ) {
 
     uint16_t rtn_val = 0;
+    uint16_t rtn_val_1 = 0;
+    uint16_t rtn_val_2 = 0;
 
     SPI_TypeDef *spi = _peripheral->spi.spi.handle.Instance;
 
-    // Write the the register address
+    // __disable_irq();  // disable all interrupts
+    // 1st ADDRESS BYTE
     while ((spi->SR & SPI_SR_TXE) == 0);
     spi->DR = reg;
     while ((spi->SR & SPI_SR_TXE) == 0);
 
-    for(int i = 2; i > 0; i--){
+    // 1st DATA BYTE
+    while ((spi->SR & SPI_SR_TXE) == 0);
+    clearRX();
+    spi->DR = 0xFF;
+    // while ((spi->SR & SPI_SR_TXE) == 0);
+    while ((spi->SR & SPI_SR_RXNE) == 0); 
+    // rtn_val |= (spi->DR << 8);
+    rtn_val_1 = (spi->DR);
 
-        // Quickly clear the RX buffer then write the 0xFF to initiate the read
-        while (spi->SR & SPI_SR_BSY){   
-            // Check RX buffer readable
-            while ((spi->SR & SPI_SR_RXNE) == 0);
-            int dummy = spi->DR;
-        }
+    // 2nd DATA BYTE
+    while ((spi->SR & SPI_SR_TXE) == 0);
+    clearRX();
+    spi->DR = 0xFF;
+    // while ((spi->SR & SPI_SR_TXE) == 0);
+    while ((spi->SR & SPI_SR_RXNE) == 0); 
+    // rtn_val |= (spi->DR);
+    rtn_val_2 = (spi->DR);
 
-        // write null 0xFF for reading
-        spi->DR = 0xFF;
-        while ((spi->SR & SPI_SR_TXE) == 0);
-        
-        // Check RX buffer readable
-        while ((spi->SR & SPI_SR_RXNE) == 0);
-
-        // Read and save data 
-        rtn_val |= (spi->DR << 8*(i-1));
-        // while ((spi->SR & SPI_SR_BSY) != 0);
-    }
-
+    // WAIT FOR TRANSACTION TO FINISH
     while ((spi->SR & SPI_SR_BSY) != 0);
+    // __enable_irq();  // disable all interrupts
+    rtn_val = (rtn_val_1 << 8) | (rtn_val_2);
     return rtn_val;
+
+    // // Write the the register address
+    // while ((spi->SR & SPI_SR_TXE) == 0);
+    // spi->DR = reg;
+    // while ((spi->SR & SPI_SR_TXE) == 0);
+
+    // for(int i = 2; i > 0; i--){
+
+    //     // Quickly clear the RX buffer then write the 0xFF to initiate the read
+    //     while (spi->SR & SPI_SR_BSY){   
+    //         // Check RX buffer readable
+    //         while ((spi->SR & SPI_SR_RXNE) == 0);
+    //         int dummy = spi->DR;
+    //     }
+
+    //     // write null 0xFF for reading
+    //     spi->DR = 0xFF;
+    //     while ((spi->SR & SPI_SR_TXE) == 0);
+        
+    //     // Check RX buffer readable
+    //     while ((spi->SR & SPI_SR_RXNE) == 0);
+
+    //     // Read and save data 
+    //     rtn_val |= (spi->DR << 8*(i-1));
+    //     // while ((spi->SR & SPI_SR_BSY) != 0);
+    // }
+
+    // while ((spi->SR & SPI_SR_BSY) != 0);
+    // return rtn_val;
 
 }
 
