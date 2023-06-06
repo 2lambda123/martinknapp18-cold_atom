1e19ab23908763f7b2f7244a31c1e8f83c010b98
G216NQ3 <martin.knapp@npl.co.uk>
2023-06-06 10:56:31 +0100

    PID seems to be working, need on the fly plotting to be sure

--- Misc/PID/PID.h
+++ Misc/PID/PID.h
@@ -1,39 +1,101 @@
-// #ifndef PID_CONTROLLER_H
-// #define PID_CONTROLLER_H
+#include "mbed.h"
 
-// typedef struct {
 
-// 	/* Controller gains */
-// 	float Kp;
-// 	float Ki;
-// 	float Kd;
+#ifndef PID_H
+#define PID_H
 
-// 	/* Derivative low-pass filter time constant */
-// 	float tau;
+// Controller Gains
+double Kp = 2;
+double Ki = 2;
+double Kd = 0;
 
-// 	/* Output limits */
-// 	float limMin;
-// 	float limMax;
-	
-// 	/* Integrator limits */
-// 	float limMinInt;
-// 	float limMaxInt;
+// Derivative low-pass filter time constant
+double tau = 1;
 
-// 	/* Sample time (in seconds) */
-// 	float T;
+// Output limits
+double limMin = -2;
+double limMax = 2;
 
-// 	/* Controller "memory" */
-// 	float integrator;
-// 	float prevError;			/* Required for integrator */
-// 	float differentiator;
-// 	float prevMeasurement;		/* Required for differentiator */
+// Integrator limits
+double limMinInt = -5;
+double limMaxInt = 5;
 
-// 	/* Controller output */
-// 	float out;
+// Sample time (in seconds)
+double T = 0.560;
 
-// } PIDController;
+// Controller "memory"
+double integrator;
+double prevError;           /* Required for integrator */
+double differentiator;
+double prevMeasurement;     /* Required for differentiator */
 
-// void  PIDController_Init(PIDController *pid);
-// float PIDController_Update(PIDController *pid, float setpoint, float measurement);
+// Controller setpoint and output
+double setpoint = 0;
+double output;
 
-// #endif
+
+// Functions
+void PIDController_Init(void){
+
+    // Clear controller variables
+    integrator = 0;
+    prevError  = 0;
+
+    differentiator  = 0;
+    prevMeasurement = 0;
+
+    output = 0;
+
+}
+
+
+double PIDController_Update(double measurement){
+
+    // Error signal
+    double error = -1*(setpoint - measurement);     // changing the sign here flips the error signal
+
+
+    // Proportional
+    double proportional = error;
+
+
+    // Integral
+    double cycle_integrator = T * (prevError + error)/2;
+    integrator += cycle_integrator;
+
+    // Anti-wind-up via integrator clamping
+    if (integrator > limMaxInt)
+    {
+        integrator = limMaxInt;
+    }
+    if (integrator < limMinInt) {
+        integrator = limMinInt;
+    }
+
+
+    // Derivative
+    differentiator = (error - prevError) / T;
+
+
+    // Compute PID output, apply limits
+    output = (Kp*proportional) + (Ki*integrator) + (Kd*differentiator);
+
+    if (output > limMax)
+    {
+        output = limMax;
+    }
+    if (output < limMin)
+    {
+        output = limMin;
+    }
+
+    // Store results to memory for future feedback
+    prevError       = error;
+    prevMeasurement = measurement;
+
+    // Return controller output
+    return output;
+
+}
+
+#endif // _PID_H_
