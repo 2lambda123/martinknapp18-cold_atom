e0418c237a81ae7cb005c0d7a023ad8ce45cf367
G216NQ3 <martin.knapp@npl.co.uk>
2023-10-31 10:55:34 +0000

    optimised detection detuning

--- Misc/settings.h
+++ Misc/settings.h
@@ -31,8 +31,8 @@ constexpr uint32_t to_AD5781dac(double voltage)
 // Variables
 
 // EXPERIMENTAL VALUES
-constexpr uint16_t LOAD_TIME = 250; // ms
-constexpr uint16_t SHOTS = 10 + 10; // plus 10 for the disregarded shots at start
+constexpr uint16_t LOAD_TIME = 250; // ms  //250
+constexpr uint16_t SHOTS = 100 + 10; // plus 10 for the disregarded shots at start
 constexpr uint16_t BG_DELAY = 8027; // us
 
 
@@ -67,8 +67,8 @@ constexpr float PGC_REPUMP_ATTE = 1.3;
 
 
 // DETECT PHASE
-constexpr float DETECT_LOCK_FREQ = 0.832;
-constexpr float DETECT_TRAP_FREQ = 1.767;           // 0.832, 1.066, 1.299, 1.533, 1.767, 2.000, 2.234*, 2.468, 2.701, 2.935, 3.169 (0->5 in 0.5 [xGAMMA])
+constexpr float DETECT_LOCK_FREQ = 0.832;           // 0.832,
+constexpr float DETECT_TRAP_FREQ = 1.066;           // 0.832, 1.066, 1.299, 1.533, 1.767, 2.000, 2.234*, 2.468, 2.701, 2.935, 3.169 (0->5 in 0.5 [xGAMMA])
 constexpr float DETECT_REPUMP_FREQ = 7.14;          // 7.14
 
 constexpr float DETECT_LOCK_ATTE = 1.3;
@@ -78,7 +78,7 @@ constexpr float DETECT_REPUMP_ATTE = 1.3;
 constexpr float OFF_REPUMP_ATTE = 0;
 constexpr float OFF_REPUMP_FREQ = 0;
 
-constexpr float DETECT_C_FIELD = 0.5;
+constexpr float DETECT_C_FIELD = 0; // 100mA/V
 
 constexpr uint8_t DROP_TIME = 5;
 
@@ -96,17 +96,19 @@ double FRACTION_ARRAY[FRACTION_ARRAY_SIZE];
 // u_WAVE PHASE
 constexpr double u_WAVE_AMP_OPEN = 0;
 constexpr double u_WAVE_AMP_CLOSE = -2;
+
+double u_WAVE_pi_power = -13.1; //-13.1 for pi pulse
 uint16_t RABI_PULSE = 5;                 // ms
 
 
 // PID locking
 double RABI_width = 0.7987 / (static_cast<double>(RABI_PULSE)/1000);
 double f0 = 9192.631770;
-// double dither_low = f0 - (RABI_width/1000000)/2;
-// double dither_high = f0 + (RABI_width/1000000)/2;
-double dither = 0.001;
-double dither_low = f0 - dither;
-double dither_high = f0 + dither;
+double dither_low = f0 - (RABI_width/1000000)/2;
+double dither_high = f0 + (RABI_width/1000000)/2;
+// double dither = 0.001;
+// double dither_low = f0 - dither;
+// double dither_high = f0 + dither;
 
 
 // Integer Values
